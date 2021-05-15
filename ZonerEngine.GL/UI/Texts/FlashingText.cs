ccb28992b64f656271b58dc296ae5be13b032a36
Niall Lewin <Lewin91@hotmail.com>
2021-05-15 17:11:30 +0100

    Missed text

---
+++ ZonerEngine.GL/UI/Texts/FlashingText.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ZonerEngine.GL.UI.Texts
+{
+    public class FlashingText : Text
+    {
+        private bool _goingUp;
+
+        public float MaxOpacity { get; set; }
+
+        public float MinOpacity { get; set; }
+
+        /// <summary>
+        /// How quickly the opacity changes
+        /// </summary>
+        public float IncrementSpeed { get; set; }
+
+        public FlashingText(SpriteFont font, StringBuilder value, float startOpacity) : base(font, value)
+        {
+            Opacity = startOpacity;
+            MaxOpacity = 1;
+            MinOpacity = 0;
+            _goingUp = true;
+            IncrementSpeed = 0.01f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (_goingUp)
+            {
+                Opacity += IncrementSpeed;
+                if (Opacity >= MaxOpacity)
+                {
+                    _goingUp = false;
+                    Opacity = MaxOpacity;
+                }
+            }
+            else
+            {
+                Opacity -= IncrementSpeed;
+                if (Opacity <= MinOpacity)
+                {
+                    _goingUp = true;
+                    Opacity = MinOpacity;
+                }
+            }
+        }
+    }
+}
---
+++ ZonerEngine.GL/UI/Texts/Text.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ZonerEngine.GL.UI.Texts
+{
+  public class Text
+  {
+    public SpriteFont Font { get; set; }
+
+    public StringBuilder Value { get; set; }
+
+    public Vector2 Position { get; set; }
+
+    public Color Colour { get; set; }
+
+    public float Opacity { get; set; }
+
+    public int Width
+    {
+      get
+      {
+        return (int)Font.MeasureString(Value).X;
+      }
+    }
+
+    public int Height
+    {
+      get
+      {
+        return (int)Font.MeasureString(Value).Y;
+      }
+    }
+
+    public Text(SpriteFont font, StringBuilder value)
+    {
+      Font = font;
+      Value = value;
+      Position = new Vector2(0, 0);
+      Colour = Color.Black;
+      Opacity = 1f;
+    }
+
+    public virtual void Update(GameTime gameTime)
+    {
+
+    }
+
+    public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+      spriteBatch.DrawString(Font, Value, Position, Colour * Opacity);
+    }
+  }
+}
